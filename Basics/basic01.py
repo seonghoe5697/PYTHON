337405aa4f84602e7402d688b5efd2aa8e1e03ec
seonghoe <225354525+seonghoe5697@users.noreply.github.com>
2025-12-25 20:24:45 +0900

    if-else문 활용1

--- Basics/basic01.py
+++ Basics/basic01.py
@@ -6,6 +6,7 @@ height = float(input("키를 m 단위로 입력하시오: "))
 bmi = weight / (height**2)
 print(f"당신의 BMI는 {bmi:.3f} 입니다.")
 
+# 출력
 # 몸무게를 kg 단위로 입력하시오: 70
 # 키를 m 단위로 입력하시오: 1.70
 # 당신의 BMI는 24.221 입니다.
