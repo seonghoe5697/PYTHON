9ca298be5fad6b30f91226b64caad032381e77bd
seonghoe <225354525+seonghoe5697@users.noreply.github.com>
2026-01-03 17:54:45 +0900

    반복문 예제

---
+++ ControlFlow/loop01.py
@@ -0,0 +1,5 @@
+dan = int(input("단 수 입력: "))
+i = 1
+while (i<=9):
+    print(f'{dan} x {i} = {dan * i}')
+    i +=1
---
+++ ControlFlow/loop02.py
@@ -0,0 +1,6 @@
+# 주사위 합이 6이되는 경우 출력
+
+for a in range(1,7):
+    for b in range(1,7):
+        if a+b==6:
+            print(f'{a} , {b}')
